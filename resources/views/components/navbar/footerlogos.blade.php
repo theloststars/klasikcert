d900bfa453a9212dc29b452a5db32816b38d615f
Nevertheless <reyzaraf@gmail.com>
2025-04-08 08:41:37 +0700

    remove email slug

--- resources/views/components/navbar/footerlogos.blade.php
+++ resources/views/components/navbar/footerlogos.blade.php
@@ -1,12 +1,7 @@
-{{-- <div class="row">
+<div class="row">
     @foreach ($footerlogos as $footlogo)
     <div class="col-lg-4 col-sm-12">
         <img class="img-fluid" src="{{ Storage::disk('public')->url($footlogo->thumbnail) }}" alt="" height="160">
     </div>
     @endforeach
-</div> --}} 
-
-@foreach ($datas as $item)
-<li><a class="menu-item" href="{{ route('services.show', $item->id) }}">{{$item->title}}</a></li>
-                                            
-@endforeach
+</div>
